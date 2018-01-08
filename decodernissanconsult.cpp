f1507d5904a9308d69e0cef56f166f2df2d0167f
Unknown <Markus.Ippy1981@gmail.com>
2018-01-08 09:12:19 +0200

    Dummy class for Nissan consult decoder

---
+++ decodernissanconsult.cpp
@@ -0,0 +1,6 @@
+#include "decodernissanconsult.h"
+
+decoderNissanConsult::decoderNissanConsult()
+{
+
+}
---
+++ decodernissanconsult.h
@@ -0,0 +1,11 @@
+#ifndef DECODERNISSANCONSULT_H
+#define DECODERNISSANCONSULT_H
+
+
+class decoderNissanConsult
+{
+public:
+    decoderNissanConsult();
+};
+
+#endif // DECODERNISSANCONSULT_H
